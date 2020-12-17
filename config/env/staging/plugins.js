c71a71bb283226f561604fdc4c5b665410360ac4
Gitesh <writemail2gitesh@gmail.com>
2020-12-17 17:35:10 +0530

    added support for cloundinary folder uploads

--- config/env/staging/plugins.js
+++ config/env/staging/plugins.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "cloudinary",
+    provider: "cloudinary-folderoptions",
     providerOptions: {
-      cloud_name: env("CLOUDINARY_CLOUD_NAME"),
-      api_key: env("CLOUDINARY_API_KEY"),
-      api_secret: env("CLOUDINARY_API_SECRET"),
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+      default_folder: env("CLOUDINARY_DEFAULT_FOLDER"),
     },
   },
   //...
