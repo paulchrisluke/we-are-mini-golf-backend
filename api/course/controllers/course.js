47527046f1bd820d2ea402c46083033f452dd04a
Gitesh <writemail2gitesh@gmail.com>
2020-12-30 11:13:57 +0530

    moved slug gen. code from controllers to models

--- api/course/controllers/course.js
+++ api/course/controllers/course.js
@@ -1,25 +1,8 @@
 "use strict";
-const slugify = require("slugify");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {
-  /**
-   * Triggered before user creation.
-   */
-  lifecycles: {
-    async beforeCreate(data) {
-      if (data.title) {
-        data.slug = slugify(data.title, { lower: true });
-      }
-    },
-    async beforeUpdate(params, data) {
-      if (data.title) {
-        data.slug = slugify(data.title, { lower: true });
-      }
-    },
-  },
-};
+module.exports = {};
--- api/course/models/course.js
+++ api/course/models/course.js
@@ -1,8 +1,22 @@
-'use strict';
+"use strict";
+const slugify = require("slugify");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.name) {
+        data.slug = slugify(data.name);
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      if (data.name) {
+        data.slug = slugify(data.name);
+      }
+    },
+  },
+};
