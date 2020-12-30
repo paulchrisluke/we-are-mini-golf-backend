df1221c186272b639de05fce5bb6e0fd604930d0
Gitesh <writemail2gitesh@gmail.com>
2020-12-30 10:51:33 +0530

    added slug for courses

--- api/course/controllers/course.js
+++ api/course/controllers/course.js
@@ -1,8 +1,25 @@
-'use strict';
+"use strict";
+const slugify = require("slugify");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Triggered before user creation.
+   */
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.title) {
+        data.slug = slugify(data.title, { lower: true });
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.title) {
+        data.slug = slugify(data.title, { lower: true });
+      }
+    },
+  },
+};
